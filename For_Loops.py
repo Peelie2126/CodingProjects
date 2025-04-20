3bfd7a13b05e56f1c02cb2c3b8969d6a2dd09db8
Peely2126 <srijan2126@gmail.com>
2025-04-20 10:54:35 -0500

    Add files via upload

---
+++ For_Loops.py
@@ -0,0 +1,8 @@
+from turtle import Turtle
+tracy=Turtle()
+for lineColor in ["red","green","blue","yellow"]:
+    tracy.pensize(5)
+    tracy.color(lineColor)
+    tracy.forward(100)
+    tracy.left(90)
+
